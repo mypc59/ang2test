f388bddfbfe6c7621ec30d3fb36347d9d56df7f4
CK <ck@CKdeMacBook-Pro.local>
2017-05-19 18:10:14 +0800

    nima

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class BlogService {
-  private name:string = "";
+  private name:string = "nima";
 
 constructor() {   
     this.name = "ck";
